9d7a3a6ef1069ec08e54c903f8a0c07610b52446
mycal <anuraggjua@gamil.com>
2025-09-24 16:27:29 +0530

    Add incredecre.dart example

---
+++ incredecre.dart
@@ -0,0 +1,12 @@
+void main() {
+  int a = 5;
+  a += 5;
+  int b = 8;
+  b -= 5;
+  int c = 7;
+  c *= 4;
+  double d = 9;
+  d /= 3;
+  print("The value of a, b, c, d is $a, $b, $c, $d");
+  return;
+}
